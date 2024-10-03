9a4d77dae7589121a5220186d61f842e0908d48a
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-10-04 06:40:59 +0700

    Update doanh thu

--- js/cms.js
+++ js/cms.js
@@ -3,7 +3,7 @@ cms.view_history_play_user=()=>{
     cr.msg_loading();
     cr_realtime.list_one("lich_su_danh_kh",datas=>{
         var list_member=[];
-        var p=cms.get_post_by_id_collection("lich_su_danh_kh");
+        let p=cms.get_post_by_id_collection("lich_su_danh_kh");
         $.each(datas,function(index,m){
             if(m.username==user.username) list_member.push(m);
         })
@@ -11,7 +11,6 @@ cms.view_history_play_user=()=>{
         list_member.sort(function(a, b) {
             return new Date(b['date']) - new Date(a['date']);
         });
-        p.label="Lịch sử chơi của ("+user.username+")";
         cms.msg_collection_data(p,["username","game","money"],list_member);
     });
 };
@@ -27,6 +26,7 @@ cms.doangthu=()=>{
         html_page+='</div>';
         html_page+='<div class="card-body">';
             html_page+='Tổ số tiền vào:<h1 id="money_vao"><i class="fas fa-spinner fa-spin"></i> đang xử lý</h1> <span class="text-muted">vnđ</span>';
+            html_page+='<p>Tổ số giao dịch : <b id="money_vao_count"><i class="fas fa-spinner fa-spin"></i> đang xử lý</b></p>';
         html_page+='</div>';
     html_page+='</div>';
     html_page+='</div>';
@@ -38,6 +38,7 @@ cms.doangthu=()=>{
     html_page+='</div>';
     html_page+='<div class="card-body">';
     html_page+='Tổ số tiền ra:<h1 id="money_ra" class="text-danger"><i class="fas fa-spinner fa-spin"></i> đang xử lý</h1> <span class="text-muted">vnđ</span>';
+    html_page+='<p>Tổ số giao dịch : <b id="money_ra_count"><i class="fas fa-spinner fa-spin"></i> đang xử lý</b></p>';
     html_page+='</div>';
     html_page+='</div>';
     html_page+='</div>';
@@ -50,6 +51,7 @@ cms.doangthu=()=>{
     html_page+='</div>';
     html_page+='<div class="card-body">';
     html_page+='Tổ doanh thu lời:<h1 id="money_tong" class="text-success"><i class="fas fa-spinner fa-spin"></i> đang xử lý</h1> <span class="text-muted">vnđ</span>';
+    html_page+='<p>Tổ số giao dịch : <b id="money_tong_count"><i class="fas fa-spinner fa-spin"></i> đang xử lý</b></p>';
     html_page+='</div>';
     html_page+='</div>';
     html_page+='</div>';
@@ -66,19 +68,29 @@ cms.doangthu=()=>{
 
     var money_vao=0;
     var money_ra=0;
+
+    var vao_count=0;
+    var ra_count=0;
+
     cr_realtime.list_one("lich_su_danh_kh",datas=>{
         $.each(datas,function(index,m){
             money_vao+=parseInt(m.money);
         });
         $("#money_vao").html(formatVND(money_vao));
+        vao_count=datas.length;
+        $("#money_vao_count").html(vao_count);
         
         cr_realtime.list_one("lich_su_chuyen_kh",datas=>{
             $.each(datas,function(index,m){
                 money_ra+=parseInt(m.money);
             });
             $("#money_ra").html(formatVND(money_ra));
+            ra_count=datas.length;
+            $("#money_ra_count").html(ra_count);
             var money_tong=money_vao-money_ra;
+            var count_pay=vao_count+ra_count;
             $("#money_tong").html(formatVND(money_tong));
+            $("#money_tong_count").html(count_pay);
         });
     });
 }
